22994a2f1e9ad2efb1bf35ce947ee4479111a324
Tal Ben-Ari <talbenari1@gmail.com>
2017-10-12 14:48:39 -0700

    Add simple error handler

--- lio-http-server/src/LIO/HTTP/Server/Frankie/Static.hs
+++ lio-http-server/src/LIO/HTTP/Server/Frankie/Static.hs
@@ -2,6 +2,7 @@
 
 module LIO.HTTP.Server.Frankie.Static (static) where
 
+import           Control.Exception          (SomeException, try)
 import           Control.Monad.Trans.Class  (lift)
 import qualified Data.ByteString.Lazy.Char8 as L8
 import           LIO.HTTP.Server.Frankie
@@ -13,8 +14,17 @@ import           System.FilePath            ((</>))
 static :: FilePath -> FilePath -> DCController ()
 static path route = do
   absPath <- lift . ioTCB $ makeAbsolute path
+  -- TODO: ensure that absPath is a prefix of fullPath
   let fullPath = absPath </> route
   log DEBUG $ "Static file lookup: " ++ fullPath
-  file <- lift . ioTCB $ L8.readFile fullPath
-  log DEBUG $ "File found: " ++ fullPath
-  respond $ okHtml file
+  fileOrEx <- lift . ioTCB . try $ L8.readFile fullPath
+  case fileOrEx of
+    Left ex -> handleEx ex
+    Right file -> do
+      log DEBUG $ "File found: " ++ fullPath
+      respond $ okHtml file
+
+handleEx :: SomeException -> DCController ()
+handleEx ex = do
+  log ERROR $ show ex
+  respond notFound
